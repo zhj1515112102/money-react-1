33d1203a0e86d0c3aab48b1757e13be7d41657f2
zhj <1515112102@qq.com>
2022-07-22 22:22:59 +0800

    完成NumberPadSection

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
+try {importAll(require.context('../icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
 type Props = {
   name: string
