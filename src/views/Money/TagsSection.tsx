87b23534b1e1935b7cc9100a5141510dee9cb264
zhj <1515112102@qq.com>
2022-07-23 11:57:45 +0800

    抽离useTags函数

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,55 +1,67 @@
 import styled from 'styled-components';
 import React, {useState} from 'react';
+import {useTags} from '../../useTags';
 
 const Wrapper = styled.section`
-  background: #FFFFFF; padding: 12px 16px;
-  flex-grow: 1; display:flex; flex-direction: column;
-  justify-content: flex-end; align-items: flex-start;
-  > ol { margin: 0 -12px;
-    > li{
-      background: #D9D9D9; border-radius: 18px;
-      display:inline-block; padding: 3px 18px;
-      font-size: 14px; margin: 8px 12px;
-      &.selected{
+  background: #FFFFFF;
+  padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  > ol {
+    margin: 0 -12px;
+    > li {
+      background: #D9D9D9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+      &.selected {
         background: #f60;
       }
     }
   }
-  > button{
-    background:none; border: none; padding: 2px 4px;
-    border-bottom: 1px solid #333; color: #666;
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
     margin-top: 8px;
   }
 `;
 type Props = {
-  selected:string[];
-  onChange:(selected:string[])=>void;
+  selected: string[];
+  onChange: (selected: string[]) => void;
 }
-const TagsSection:React.FC<Props> =(props)=>{
-  const [tags,setTags] = useState<string[]>(['衣','食','住','行'])
-  const selectedTags = props.selected
-  const onAddTag=()=>{
-    const tagName= window.prompt('新标签的名称为')
-    if(tagName!==null){
-      setTags([...tags,tagName])
+const TagsSection: React.FC<Props> = (props) => {
+  const {tags,setTags} = useTags()
+  const selectedTags = props.selected;
+  const onAddTag = () => {
+    const tagName = window.prompt('新标签的名称为');
+    if (tagName !== null) {
+      setTags([...tags, tagName]);
     }
-  }
-  const onToggleTag=(tag:string)=>{
-    const index = selectedTags.indexOf(tag)
-    if(index>=0){
-      props.onChange(selectedTags.filter(t=>t!==tag))
+  };
+  const onToggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag);
+    if (index >= 0) {
+      props.onChange(selectedTags.filter(t => t !== tag));
       // 如果tag被选中，就复制没有被选中的tag，作为新的selectedTag
-    }else{
-      props.onChange([...selectedTags,tag])
+    } else {
+      props.onChange([...selectedTags, tag]);
     }
-  }
-  const getClass =(tag:string)=>selectedTags.indexOf(tag)>=0?'selected':''
-  return(
+  };
+  const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
+  return (
     <Wrapper>
       <ol>
-        {tags.map(tag=>
+        {tags.map(tag =>
           <li key={tag}
-              onClick={()=>onToggleTag(tag)}
+              onClick={() => onToggleTag(tag)}
               className={getClass(tag)}
           >
             {tag}
@@ -58,8 +70,8 @@ const TagsSection:React.FC<Props> =(props)=>{
       </ol>
       <button onClick={onAddTag}>新增标签</button>
     </Wrapper>
-  )
-}
+  );
+};
 
 
 export {TagsSection};
