2ac3b26f0b016b8edf3d09caa108329ed331faa8
zhj <1515112102@qq.com>
2022-07-24 20:24:42 +0800

    tag list 样式做完

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,12 +1,55 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import {useTags} from '../useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
 
+const TagList = styled.ol`
+  font-size: 16px;
+  >li{
+    border-bottom: 1px solid #D5D5D9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #f60;
+  border-radius: 4px;
+  color: white;
+`
+const  Center = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const Space = styled.div`
+  height: 16px;
+`
 function Tags() {
   const {tags,setTags} = useTags()
   return (
     <Layout>
-      <h2>标签页面</h2>
+      <TagList>
+        {tags.map(tag=>
+        <li key={tag}>
+          <span className="oneLine">{tag}</span>
+          <Icon name='right'/>
+        </li>)}
+      </TagList>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 }
