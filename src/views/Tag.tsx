00f78852b960485daf73f4714c2881d3af75c469
zhj <1515112102@qq.com>
2022-07-24 22:38:04 +0800

    封装 findTaf 到 useTags里

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,7 +1,15 @@
+import {useTags} from '../useTags';
+import {useParams} from 'react-router-dom';
 
-const Tag=()=>{
-  return(
-    <div>1111</div>
-  )
+type Params = {
+  id: string
 }
-export {Tag}
+const Tag: React.FC = (props) => {
+  const {findTag} = useTags();
+  let {id} = useParams<Params>();
+  const tag = findTag(parseInt(id));
+  return (
+    <div>{tag.name}</div>
+  );
+};
+export {Tag};
